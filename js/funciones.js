df62be3b91b5814d9b7178db0b7246d41ef3870c
unknown <you@example.com>
2020-07-28 18:41:06 -0400

    “HildaRolon”

---
+++ js/funciones.js
@@ -0,0 +1,9 @@
+function redireccionar (){
+    
+    window.location="https://www.facebook.com/Hilda-Rol%C3%B3n-fotograf%C3%ADa-426190518137672/?__tn__=%2Cd%2CP-R&eid=ARBBotvNy5Ar4XnR9ka7UZAaCaJ_DBcbYNpHyTyojN2otOV4x6L0MnIJDNeMQBfvNr2Ch9LXmV3-vpTi";
+    
+}
+function redireccionar_instagram(){
+    
+    window.location = "https://www.instagram.com/hildarolon26/";
+}
